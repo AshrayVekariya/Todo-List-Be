f98cfacea32d40723d0f5ca2d1087a0006d63840
AshrayVekariya <ashrayvekariya.vision@gmail.com>
2024-05-06 12:43:14 +0530

    Changes private keys and api

--- firebase/index.js
+++ firebase/index.js
@@ -1,9 +1,19 @@
 const firebase = require('firebase-admin');
 
-const serviceAccount = require('../firebase.config.json');
-
 firebase.initializeApp({
-    credential: firebase.credential.cert(serviceAccount)
+    credential: firebase.credential.cert({
+        "type": "service_account",
+        "project_id": "push-notification-cf089",
+        "private_key_id": process.env.private_key_id,
+        "private_key": process.env.private_key,
+        "client_email": process.env.client_email,
+        "client_id": process.env.client_id,
+        "auth_uri": process.env.auth_uri,
+        "token_uri": process.env.token_uri,
+        "auth_provider_x509_cert_url": process.env.auth_provider_x509_cert_url,
+        "client_x509_cert_url": process.env.client_x509_cert_url,
+        "universe_domain": "googleapis.com"
+    })
 })
 
 // send notification
